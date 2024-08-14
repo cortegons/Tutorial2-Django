1d9bfe3bcac724846305fa53649f2b1546e87555
Camilo <camilorte1030@gmail.com>
2024-08-14 07:42:21 -0500

    agregada about y home

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,25 @@
+from typing import Any
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from django.views.generic import TemplateView
 # Create your views here.
 
-def homePageView(request):
-    return HttpResponse('Hello World')
+#def homePageView(request):
+#    return HttpResponse('Hello World')
+
+class homePageView(TemplateView):
+    template_name = "home.html"
+
+
+class AboutPageView(TemplateView):
+    template_name = "pages/about.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            "title": "About us - Online Store",
+            "subtitle": "About us",
+            "description": "This is an about page...",
+            "author": "Developed by: Camilo Ortegon",
+        })
+        
+        return context
